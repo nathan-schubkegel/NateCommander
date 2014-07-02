72407af09a560d391cbba9ac313adc6d05341840
nathschu <nathan.schubkegel@gmail.com>
2014-07-01 22:15:16 -0800

    FatalErrorHandler void return values

--- FatalErrorHandler.c
+++ FatalErrorHandler.c
@@ -3,14 +3,14 @@
 #include <windows.h>
 #include "SDL.h"
 
-int FatalError(const char * message)
+void FatalError(const char * message)
 {
   MessageBox(0, message, "NateCommander Fatal Error", 0);
   SDL_Quit();
   exit(-1);
 }
 
-int FatalError2(const char* message, const char * extraInfo)
+void FatalError2(const char* message, const char * extraInfo)
 {
   MessageBox(0, message, "NateCommander Fatal Error", 0);
   MessageBox(0, extraInfo, "NateCommander Supplemental Info to Error", 0);
--- FatalErrorHandler.h
+++ FatalErrorHandler.h
@@ -1,7 +1,7 @@
 #ifndef NATE_COMMANDER_FATAL_ERROR_HANDLER
 #define NATE_COMMANDER_FATAL_ERROR_HANDLER
 
-int FatalError(const char * message);
-int FatalError2(const char * message, const char * extraInfo);
+void FatalError(const char * message);
+void FatalError2(const char * message, const char * extraInfo);
 
 #endif
